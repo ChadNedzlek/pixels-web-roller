fffcd6a52e9881641eeedce0c3197cb12b09cea9
ChadNedzlek <chadnedz@microsoft.com>
2023-06-27 21:43:09 -0700

    Missed a few files

---
+++ PixelsBlazorInterop/IPixelDevice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PixelsBlazorInterop;
+
+public interface IPixelDevice : IAsyncDisposable
+{
+    event Action<IPixelDevice, string> RollingStateChanged;
+    event Action<IPixelDevice> Disconnected;
+    
+    string RollState { get; }
+    int Face { get; }
+    long PixelId { get; }
+    string Name { get; }
+
+    Task ConnectAsync();
+}
